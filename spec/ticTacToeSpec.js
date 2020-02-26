fae8d34500a47bbba50ebc9bc387f82daa1e0337
Lucian Boatright <lucian.boatright@gmail.com>
2020-02-26 17:39:30 +0000

    added html board with mark ability

---
+++ spec/ticTacToeSpec.js
@@ -0,0 +1,181 @@
+
+// 'use strict';
+// describe ('playersMark', function() {
+//   var player;
+
+//   beforeEach(function(){
+//     player = new playersMark();
+//   });
+//   it('returns player choice ', function() {
+//     expect(player.getCurrentMark()).toEqual("X")
+//   });
+
+
+
+
+
+
+
+
+
+
+// // 'use strict';
+// describe ('Thermostate', function() {
+//   var thermostat;
+
+//   beforeEach(function(){
+//     thermostat = new Thermostat();
+//   });
+//   it('it returns temperature (20)', function() {
+//     expect(thermostat.getCurrentTemperature()).toEqual(20)
+//   });
+//   it('increases temperature by (1)', function() {
+//     thermostat.up()
+//     expect(thermostat.getCurrentTemperature()).toEqual(21)
+//   });
+//   it('decreases temperature by (1)', function() {
+//     thermostat.down()
+//     expect(thermostat.getCurrentTemperature()).toEqual(19)
+//   });
+//   it('minimum temperature is (10)', function() {
+//     for (var i=0; i<11; i++) {
+//       thermostat.down()
+//     }
+//     expect(thermostat.getCurrentTemperature()).toEqual(10)
+//   });
+//   it('test that powerSaveOn return true', function(){
+//     expect(thermostat.powerSaveMode).toEqual(true);
+//     thermostat.powerSaveSwitch();
+//     expect(thermostat.powerSaveMode).toEqual(false);
+//   })
+//   it('if powerSaveMode[true] max temp(25) or powerSaveMode[false] min temp(32)', function(){
+//     for (var i=0; i<25; i++) {
+//       thermostat.up()
+//     }
+//     expect(thermostat.getCurrentTemperature()).toEqual(25);
+//     thermostat.powerSaveSwitch();
+//     for (var i=0; i<25; i++) {
+//       thermostat.up()
+//     }
+//     expect(thermostat.getCurrentTemperature()).toEqual(32);  
+//   })
+//   it('when temperature is reset thermostat shows 20', function() {
+//     for (var i=0; i<25; i++) {
+//       thermostat.up()
+//     }
+//     thermostat.resetTemp()
+//     expect(thermostat.getCurrentTemperature()).toEqual(20);
+//   });
+//   it('if temperature is 17 or lower, energy usage equals low-usage', function () {
+//     for (var i=0; i<10; i++) {
+//       thermostat.down()
+//     }
+//     thermostat.energyUsage()
+//     expect(thermostat.usage).toEqual("low-usage")
+//   });
+//   it('if temperature is between 18-25, energy usage equals mid-usage', function () {
+//     expect(thermostat.usage).toEqual("mid-usage")
+//   });
+//   it('if temperature is higher than 25, energy usage equals high-usage', function () {
+//     thermostat.powerSaveSwitch()
+//     for (var i=0; i<30; i++) {
+//       thermostat.up()
+//     }           
+//     thermostat.energyUsage()
+
+
+//     expect(thermostat.usage).toEqual("high-usage")
+//   });
+
+//   // it('when power save mode is [on] maximum temp is (25)', function(){
+//   //   thermostat.power_save = true
+//   //   for (var i=0; i<30; i++) {
+//   //     thermostat.up()
+//   //   }
+//   //   expect(thermostat.getCurrentTemperature()).toEqual(25)
+//   // });
+// });
+
+
+// function Thermostat(){
+//   this.temperature = 20;
+//   this.powerSaveMode = true;
+//   this.usage = "mid-usage";
+// };
+
+// Thermostat.prototype.getCurrentTemperature = function() {
+//   return this.temperature;
+// };
+
+// Thermostat.prototype.up = function() {
+//   if (this.powerSaveMode === true && this.temperature === 25){
+//     return this.temperature;
+//   }
+//   else if (this.powerSaveMode === false && this.temperature === 32){
+//     return this.temperature;
+//   }
+//   else {
+//     return this.temperature += 1
+//   }
+  
+// };
+
+// Thermostat.prototype.down = function() {
+//   if (this.temperature === 10) {
+//     return this.temperature
+//   } else {
+//     return this.temperature -= 1;
+//   }
+// };
+
+// Thermostat.prototype.powerSaveSwitch = function() {
+//   this.powerSaveMode = !this.powerSaveMode
+// };
+
+// Thermostat.prototype.resetTemp = function() {
+//   return this.temperature = 20;
+// };
+
+// Thermostat.prototype.energyUsage = function() {
+
+//   if (this.temperature < 18){
+//     this.usage = 'low-usage';
+//   } 
+//   else if (this.temperature > 17 && this.temperature < 25){
+//     this.usage = 'mid-usage';
+//   }
+//   else {
+//     this.usage = 'high-usage';
+//   }
+// };
+
+
+
+//   // if (this.power_save === true)
+//   //   if (this.temperature === 25){
+//   //     reutrn this.temperature
+//   //   }
+//   //   else if {this.temperature < 25
+//   //     return this.temperature += 1;
+//   //   }
+//   // else if (this.power_save === false)
+//   //   if (this.temperature === 32){
+//   //     return this.temperature
+//   //   }
+//   //   else {
+//   //     return this.temperature += 1;
+//   //   }
+// // function Thermostat(temp) = {
+// //   this.temperature = temp;
+// // };
+
+// // Thermostat.prototype.show = function(){
+// //   return this.temperature;
+// // };
+
+// // var test_thermo = new Thermostat(20);
+
+// // test_thermo.temp();
+// // Thermostat.prototype.temperature = function(){
+// //   reutrn this.temperature;
+// // };
---
+++ src/ticTacToe.js
@@ -0,0 +1,76 @@
+
+// $(document).ready(function(){
+//   var firstload
+
+//   var player = new Player();
+  
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <!-- <!DOCTYPE html>
+// <html>
+// <head>
+//   <meta charset="utf-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <title>Thermostate</title>
+//   <script src="jquery-3.4.1.js"></script>
+// </head>
+// <body>
+//   <select>
+//     <h1 id="temperature"></h1>
+//     <p>
+//       <button id="temperature-up">+</button>
+//       <button id="temperature-down">-</button>
+//       <button id="temperature-reset">reset</button>
+//       power saving mode is <<span id="power-saving-status">on</span>
+//       <button id="powersaving-on">PSM on</button>
+//       <button id="powersaving-off">PSM off</button>
+//     </p>
+//   </select>
+//   <button id="add">Click-me</button>
+
+
+
+//   <script>
+//     $(function(){
+//       $("#add").on('click',function(){
+//         var $newItem = $('<li>NEW ITEM</li>')
+//         $newItem.appendTo('.items');
+//         // alert($(this).text("HELLO"));
+//       });
+//       $('.items').on('click', 'li', function(){
+//         $(this).fadeOut(500);
+//       });
+//       $('li').click(function(event){
+//         console.log('clicked', $(this).text());
+//       });
+//       $('li').on('click.logging', function(){
+//         console.log('A list iem was clicked')
+//       });
+//       $('li').on('click.analytics', function(){
+//         registerClick();
+//         doSomethingElse();
+//       });
+//       $('li').off('click.logging');
+//     });
+//   </script>
+  
+// </body>
+// </html>
+//  -->
